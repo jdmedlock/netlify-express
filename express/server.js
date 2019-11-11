f5b4e82d141c7357e8cecb0b65f5f2a73146c67f
jdmedlock <jdmedlock@gmail.com>
2019-11-11 10:34:21 -0600

    Refactor: Remove server initialization from Sherpabot .then
    Remove the server initialization code from the Sherpabot Promise
    resolution code.
    
    Resolves: N/a
    See also: N/a

--- express/server.js
+++ express/server.js
@@ -1,5 +1,5 @@
 'use strict';
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
@@ -8,26 +8,25 @@ const sherpabot = require("./sherpabot");
 
 const app = express();
 const router = express.Router();
-/*
+
 const result = dotenv.config();
 if (result.error) {
   throw result.error
 }
-*/
-sherpabot().then((value) => {
-  router.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
-  });
-  router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-  router.post('/', (req, res) => res.json({ postBody: req.body }));
-  
-  app.use(bodyParser.json());
-  app.use('/.netlify/functions/server', router);  // path must route to lambda
-  app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-})
 
+sherpabot();
+
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
